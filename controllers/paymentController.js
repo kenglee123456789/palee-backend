fb8dccea8d9ccff1c746aa86bb8cca0a4ef0c136
kenglee123456789 <inew86023@gmail.com>
2025-04-20 13:31:12 +0700

    first

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -1,14 +1,15 @@
 const paymentModel = require('../models/paymentModel');
 
-const createPayment = async (req, res) =>{
-    const {regisID, amount, pay, pend, date} = req.body;
+const createPayment = async(req, res) => {
+    const { regisID, amount, pend, date } = req.body;
+    const pay = 0;
     try {
         await paymentModel.addPayment(regisID, amount, pay, pend, date);
-        res.status(200).send({message: 'Create payment successful'})
+        res.status(200).send({ message: 'Create payment successful' })
     } catch (error) {
         console.error('Error', error);
-        res.status(500).send({message: 'Create payment failed'});
+        res.status(500).send({ message: 'Create payment failed' });
     }
 }
 
-module.exports = {createPayment}
+module.exports = { createPayment }
