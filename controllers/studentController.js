bd220c5dcdae35362ce97996b03333bb4ff7100f
kenglee123456789 <148339885+kenglee123456789@users.noreply.github.com>
2025-04-27 09:04:31 +0700

    Update studentController.js

--- controllers/studentController.js
+++ controllers/studentController.js
@@ -1,30 +1,63 @@
-
-const studentModel = require('../models/studentModel');
-
+const studentModel = require("../models/studentModel");
 const getNextStudentID = async (req, res) => {
   try {
     const results = await studentModel.getLastStudentID();
-    let nextID = 'STD001';
+    let nextID = "STD001";
     if (results.length > 0) {
       let lastStdID = results[0].stdID;
-      let currentNum = parseInt(lastStdID.replace('STD', ''));
+      let currentNum = parseInt(lastStdID.replace("STD", ""));
       currentNum++;
-      nextID = 'STD' + currentNum.toString().padStart(3, '0');
+      nextID = "STD" + currentNum.toString().padStart(3, "0");
     }
     res.json({ nextStudentID: nextID });
   } catch (err) {
-    res.status(500).send({ message: 'Error retrieving students data' });
+    res.status(500).send({ message: "Error retrieving students data" });
   }
 };
 
 const addStudent = async (req, res) => {
-  const { studentID, studentName, studentLastname, gender, tel, phone, school, district, stayID } = req.body;
+  const {
+    studentID,
+    studentName,
+    studentLastname,
+    gender,
+    tel,
+    phone,
+    school,
+    district,
+    stayID,
+  } = req.body;
+
   try {
-    await studentModel.addStudent(studentID, studentName, studentLastname, gender, tel, phone, school, district, stayID);
-    res.status(200).send({ message: 'Student added successfully!' });
+    if ((gender === "1" || gender === "3") && stayID === "2") {
+      const count = await studentModel.countMaleInStay2();
+      if (count >= 100) {
+        return res.status(400).send({ message: "Male dorm full" });
+      }
+    }
+
+    if (gender === "2" && stayID === "2") {
+      const count = await studentModel.countFeMaleInStay2();
+      if (count >= 50) {
+        return res.status(400).send({ message: "FeMale dorm full" });
+      }
+    }
+
+    await studentModel.addStudent(
+      studentID,
+      studentName,
+      studentLastname,
+      gender,
+      tel,
+      phone,
+      school,
+      district,
+      stayID
+    );
+    res.status(200).send({ message: "Student added successfully!" });
   } catch (err) {
-    console.error('Error inserting student data: ', err);
-    res.status(500).send({ message: 'Error inserting student data' });
+    console.error("Error inserting student data: ", err);
+    res.status(500).send({ message: "Error inserting student data" });
   }
 };
 
