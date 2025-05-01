aeaa682572bc7bed3f19b6c024dac23bc177a2f5
kenglee123456789 <148339885+kenglee123456789@users.noreply.github.com>
2025-05-01 17:56:29 +0700

    Update regisdetailModel.js

--- models/regisdetailModel.js
+++ models/regisdetailModel.js
@@ -2,7 +2,7 @@ const db = require('../config/db')
 
 const addRegistdetail = (regisID, subject, scholarship, Cost) =>{
     return new Promise((resolve, reject)=>{
-      db.query('INSERT INTO regisdetail VALUES (?, ?, ? ,?)',[regisID, subject, scholarship, Cost],
+      db.query('INSERT INTO regisdetail(regisID, subDetailID, scholarship_id, cost) VALUES (?, ?, ? ,?)',[regisID, subject, scholarship, Cost],
         (error, results)=>{
           if(error) reject(error);
           resolve(results);
