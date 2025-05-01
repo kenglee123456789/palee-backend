ad2fd50dfa9fb13b38fbc35a5b36b530a8add37e
kenglee123456789 <148339885+kenglee123456789@users.noreply.github.com>
2025-05-01 14:34:19 +0700

    Update app.js

--- app.js
+++ app.js
@@ -11,18 +11,14 @@ const billRoutes = require('./routes/billRoute')
 const registrationRoutes = require('./routes/registrationRoute')
 const regisdetailRoutes = require('./routes/regisdetailRoutes')
 const paymentRoutes = require('./routes/paymentRoutes')
-const allow = ['https://palee-center.netlify.app'];
+const allow = [];
 const app = express();
 const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors(
-  origin: allow,
-  methods: [
-  'post', 'put', 'delete', 'get'
-  ],
-credentials: false,
+  {origin: 'https://palee-center.netlify.app'}
 ));
 
 app.use('/api', studentRoutes);
