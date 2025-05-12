64b51b56d37c955bb0abab5872ea7b4ea6cd2455
kenglee123456789 <148339885+kenglee123456789@users.noreply.github.com>
2025-05-12 18:20:19 +0700

    Update regisdetailModel.js

--- models/regisdetailModel.js
+++ models/regisdetailModel.js
@@ -10,4 +10,13 @@ const addRegistdetail = (regisID, subject, scholarship, Cost) =>{
       )
     })
   }
-  module.exports = {addRegistdetail}
+const getRegisdetail =()=>{
+    return new Promise((resolve, reject) => {
+        db.query('SELECT rd.regisID, std.stdName,std.Lastname, s.subName, l.level FROM regisdetail rd INNER JOIN registration r ON rd.regisID = r.regisID INNER JOIN student std ON r.stdID = std.stdID INNER JOIN subjectDetail subdt ON rd.subDetailID = subdt.subDetailID INNER JOIN subject s ON subdt.subID = s.subID INNER JOIN level l ON subdt.levelID = l.levelID',
+            (error, results) => {
+                if (error) reject(error);
+                resolve(results);
+            });
+    });
+}
+  module.exports = {addRegistdetail, getRegisdetail}
