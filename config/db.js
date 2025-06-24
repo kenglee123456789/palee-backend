c74843666550958441104fdf806a0c6f56430962
kenglee123456789 <148339885+kenglee123456789@users.noreply.github.com>
2025-06-24 21:23:03 +0700

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,21 +1,17 @@
-// models/db.js
 const mysql = require('mysql2');
 const dotenv = require('dotenv');
 dotenv.config();
-const db = mysql.createConnection({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
+
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
 
-db.connect((err) => {
-    if (err) {
-        console.error('Error connecting to the database: ', err);
-        return;
-    }
-    console.log('Connected to the MySQL database');
-});
+module.exports = pool.promise();
 
-module.exports = db;
